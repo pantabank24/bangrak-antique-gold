e6bae70bc6f7e7e1e7480d6abc2be3fb69de4652
i am atomic <pantabank@gmail.com>
2026-07-15 22:13:15 +0700

    First Initial.

---
+++ components/navbar.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+
+interface Props {
+  isTransparent?: boolean;
+}
+
+const ModernNavbar = ({ isTransparent }: Props) => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  // Navbar scroll effect
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrolled = window.scrollY > 200;
+      setIsScrolled(scrolled);
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <nav
+      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300     ${isTransparent
+        ? isScrolled
+          ? "bg-[#C3A88F]/80 backdrop-blur-lg shadow-lg border-b border-yellow-500"
+          : "bg-transparent"
+        : "bg-[#C3A88F]/80 backdrop-blur-lg shadow-lg border-b border-yellow-500"
+        }`}
+    >
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          {/* Logo Brand */}
+          <div className="flex items-center gap-3">
+            {isTransparent ? (
+              isScrolled ? (
+                <div className=" flex flex-row items-center">
+                  <img src="/images/JKLOGO.png" alt="logo" className=" w-8" />
+                  <div className=" flex flex-col">
+                    <span className="font-bold  text-xl ml-3 bg-gradient-to-b from-yellow-100 via-yellow-400 to-yellow-600 bg-clip-text text-transparent">
+                      บางรัก แอนทีค
+                    </span>
+                    <span className="font-bold  text-xs ml-3 mt-[-5] bg-gradient-to-b from-yellow-100 to-yellow-600 bg-clip-text text-transparent">
+                      รับเช่าพระเครื่อง-พระบูชา ของเก่าโบราณ
+                    </span>
+                  </div>
+                </div>
+              ) : null
+            ) : (
+              <div className=" flex flex-row items-center">
+                <img src="/images/JKLOGO.png" alt="logo" className=" w-8" />
+                <div className=" flex flex-col">
+                  <span className="font-bold  text-xl ml-3 bg-gradient-to-b from-yellow-100 via-yellow-400 to-yellow-600 bg-clip-text text-transparent">
+                    บางรัก แอนทีค
+                  </span>
+                  <span className="font-bold  text-xs ml-3 mt-[-5] bg-gradient-to-b from-yellow-100 to-yellow-600 bg-clip-text text-transparent">
+                    รับซื้อ - หลอม ทอง เงิน นาค
+                  </span>
+                </div>
+              </div>
+            )}
+          </div>
+
+          {/* Social Links */}
+          <div className="flex items-center gap-4 bg-white/20  backdrop-blur-sm border border-white/20 px-3 py-1 rounded-full">
+            <a
+              href="https://web.facebook.com/profile.php?id=61567928181159&_rdc=1&_rdr"
+              className="w-8 h-8 flex items-center justify-center rounded-full   hover:bg-white/20 transition-colors"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="/images/facebook.png"
+                alt="facebook"
+                className="w-6 h-6"
+              />
+            </a>
+            <a
+              href="https://page.line.me/buybangrak"
+              className="w-8 h-8 flex items-center justify-center rounded-full  hover:bg-white/20 transition-colors"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="/images/line.png" alt="line" className="w-6 h-6" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default ModernNavbar;
---
+++ config/site.ts
@@ -0,0 +1,47 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "บางรัก แอนทีค | เช็คราคาทอง ตรวจราคาทอง ประเมินราคาทอง",
+  url: "https://example.com/",
+  description: "บางรัก แอนทีค | รับเช่าพระเครื่อง-พระบูชา รับซื้อของเก่าของโบราณ และกระเพาะปลาเก่า แอดไลน์ https://page.line.me/buybangrak โทร 0969128888 หรือเดินทางมาที่ร้าน https://maps.app.goo.gl/W2riCWFrxUn8LC6F8",
+  navItems: [
+  ],
+  navMenuItems: [
+    {
+      label: "Profile",
+      href: "/profile",
+    },
+    {
+      label: "Dashboard",
+      href: "/dashboard",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Team",
+      href: "/team",
+    },
+    {
+      label: "Calendar",
+      href: "/calendar",
+    },
+    {
+      label: "Settings",
+      href: "/settings",
+    },
+    {
+      label: "Help & Feedback",
+      href: "/help-feedback",
+    },
+    {
+      label: "Logout",
+      href: "/logout",
+    },
+  ],
+  links: {
+    line: "https://page.line.me/buybangrak",
+    facebook: "https://web.facebook.com/profile.php?id=61567928181159&_rdc=1&_rdr"
+  },
+};
